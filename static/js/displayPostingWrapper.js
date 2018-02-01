45d23baca68a6e2e9fc5ff4b4eaa4888be269d9b
117chan <117chan@posteo.de>
2018-02-01 16:26:45 -0500

    Got script working successfully to show/hide new thread creation. Needs some polishing.

--- static/js/displayPostingWrapper.js
+++
@@ -1,8 +0,0 @@
-function displayFunction() {
-    var x = document.getElementById("displayFormWrapper");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
