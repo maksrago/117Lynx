53d6f0819140834b86137d85abf6f91069613ec8
117chan <117chan@posteo.de>
2018-02-18 20:23:19 -0500

    Added donation page, with 3 different methods of donation. Fixed nav bar, and several other small bugs.

--- static/js/buttonInject.js
+++ static/js/buttonInject.js
@@ -2,7 +2,7 @@ var buttonContent;
 var buttonBlock;
 buttonContent = document.createElement('div');
 
-buttonContent.innerHTML = '<div class = "linkBoxes"><ul><li class = "first"><a href="/">Home</a></li><li><a href="/.static/faq.html">FAQ</a></li><li><a href="/.static/globalRules.html">Rules</a></li><li><a href="/.static/contact.html">Contact</a></li><li><a href="/logs.js">Logs</a></li><li><a href="/graphs.js">Daily Graphs</a></li><li><a href="/account.js">Account</a></li><ul></div>'
+buttonContent.innerHTML = '<div class = "linkBoxes"><ul><li class = "first"><a href="/">Home</a></li><li><a href="/.static/faq.html">FAQ</a></li><li><a href="/.static/globalRules.html">Rules</a></li><li><a href="/.static/contact.html">Contact</a></li><li><a href="/.static/donate.html">Donate</a></li><li><a href="/logs.js">Logs</a></li><li><a href="/graphs.js">Daily Graphs</a></li><li><a href="/account.js">Account</a></li><ul></div>'
 
 buttonBlock = document.getElementById('buttonType');
 buttonBlock.appendChild(buttonContent);
