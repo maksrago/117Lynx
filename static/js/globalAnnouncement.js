617d9c6619ea79762aead6f950d156fa3efd732f
117chan <117chan@posteo.de>
2018-02-23 05:49:07 -0500

    Few adjustments to rules.

--- static/js/globalAnnouncement.js
+++ static/js/globalAnnouncement.js
@@ -1,5 +1,5 @@
 announce = [];
-announce[0] = "Janitor applications are now open! Click <a href = \"/.static/janitor.html\">here</a> to apply.";
+announce[0] = "Janitor applications are now open! Click <a href = \"/.static/janitor.html\">here</a> to apply. <br />Loli/Shota has been unbanned.";
 
 function globalAnnounce() {
     document.getElementById("globalAnnouncement").innerHTML += (announce[0]);
